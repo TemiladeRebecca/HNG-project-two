a9d070cb1c973711ea1b7e421cd5afbe4ec5205e
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-07-14 04:30:25 +0100

    My App functions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['api.timbu.cloud'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.timbu.cloud',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
   },
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['api.timbu.cloud'],
+//   },
+// };
+
+// export default nextConfig;
