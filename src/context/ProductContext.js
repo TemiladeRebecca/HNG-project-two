468336b83db47ef0570019410f80d435e9fef8bd
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-07-14 04:24:49 +0100

    Folder containing API request function and contexts used in the app

---
+++ src/context/ProductContext.js
@@ -0,0 +1,40 @@
+'use client';
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { fetchProductsData } from '@/src/pages/handleFetchData';
+
+const ProductContext = createContext();
+
+export default function ProductProvider({ children }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadProducts() {
+      try {
+        const fetchedProducts = await fetchProductsData();
+        setProducts(fetchedProducts.items);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadProducts();
+  }, []);
+
+  useEffect(() => {
+    // This will log the products state whenever it updates
+    console.log(products);
+  }, [products]);
+
+  return (
+    <ProductContext.Provider value={{ products, loading, error }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export const useProductContext = () => useContext(ProductContext);
